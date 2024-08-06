6337d1f144c63177c3964e13a1c7d06eeb803b63
Gautam <gautam.mca23.du@gmail.com>
2024-08-06 20:29:31 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='flex text-white font-bold  bg-black'>
+        This is  made by Gautam 
+    </footer>
+  )
+};
+
+export default Footer;
